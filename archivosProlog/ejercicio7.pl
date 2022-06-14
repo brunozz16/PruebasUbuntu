fc6a001c7864252e97f823c45d5a7fb9227ea7a5
brunozz16 <105996135+brunozz16@users.noreply.github.com>
2022-06-14 13:09:50 -0300

    14-06

--- archivosProlog/ejercicio7.pl
+++ archivosProlog/ejercicio7.pl
@@ -1,4 +1,15 @@
+menu('Bombones de jamón','Locro','Dulce de batata').
+menu('Bombones de jamón', 'Locro', 'Alfajor norteño').
+menu('Tarta de Atún', 'Atados de repollo', 'Dulce de batata').
+menu('Tarta de Atún', 'Pollo romano con hierbas y vino', 'Flan').
+menu('Volovanes de atún', 'Matambre con espinacas y parmesano', 'Torta moka').
+menu('Buñuelos de bacalao', 'Pollo romano con hierbas y vino', 'Alfajor norteño').
+entrada("omelete").
+entrada("empanada").
+principal("locro").
+principal("guiso").
+postre("flan").
+postre("helado").
 
-carta(Entrada,Principal,Postre):-menu
-
-%deberia retornar 3³ resultados posibles
+carta(Entrada,Principal,Postre):- entrada(Entrada),principal(Principal),not(postre(Postre))
+%carta(Entrada,Principal,Postre):-menu(Entrada,_,_),menu(_,Principal,_),menu(_,_,Postre).
